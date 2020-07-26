4b40adf11f3ac6727f94616a9768b33a8f0c3e01
meniality <patrh_99@hotmail.com>
2020-07-26 15:07:24 -0600

    added a routes folder and item file in routes. Created a GET that sorts and returns all items in db.

---
+++ routes/api/items.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+// Item Model
+const Item = require('../../models/Item')
+
+// @route GET api/items
+// @desc  Get All Items
+// @access  Public
+
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({date: -1})
+    .then(items => res.json(items))
+})
+
+// @route POST api/items
+// @desc  Create a Post
+// @access  Public
+
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({date: -1})
+    .then(items => res.json(items))
+})
+
+module.exports = router
