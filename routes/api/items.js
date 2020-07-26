9d482faec5237e1ba54ed098ef0e511e54cd258d
meniality <patrh_99@hotmail.com>
2020-07-26 15:19:00 -0600

    added post and delete methods to items routes. added catch for delete that give a 404 if id of item is not fouind.

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,13 +15,26 @@ router.get('/', (req, res) => {
 })
 
 // @route POST api/items
-// @desc  Create a Post
+// @desc  Create an Item
 // @access  Public
 
-router.get('/', (req, res) => {
-  Item.find()
-    .sort({date: -1})
-    .then(items => res.json(items))
+router.post('/', (req, res) => {
+   const newItem = new Item({
+     name:req.body.name
+   })
+
+   newItem.save()
+    .then(item => {res.json(item)})
+})
+
+// @route DELETE api/items/:id
+// @desc  Delete an Item
+// @access  Public
+
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(()=> res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}))
 })
 
 module.exports = router
