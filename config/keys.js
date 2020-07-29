92c073d73ebba373587bf6a54e08a3588beb75b1
meniality <patrh_99@hotmail.com>
2020-07-29 10:36:02 -0600

    added auth route. Route checks if entered email is found then compares hased password against stored hash.

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://patrick:Patrick@mernshoppinglist.dhi65.mongodb.net/test?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const config = require('config')
 const path = require('path')
 
-const items = require('./routes/api/items');
 
 const app = express();
 
 //Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // connect to mongo
 mongoose
-  .connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err)) 
 
 // use routes
-app.use('/api/items', items)
+app.use('/api/items', require('./routes/api/items'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 // Serve static assets if in production
 if(process.env.NODE_ENV==='production'){
