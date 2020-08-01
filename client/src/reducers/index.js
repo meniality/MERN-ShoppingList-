66b77efc437db1937a77deca4487aa6ec5c83906
meniality <patrh_99@hotmail.com>
2020-08-01 13:07:17 -0600

    added reducer for error handleing in redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import {combineReducers} from 'redux'
 import itemReducer from './itemReducer'
+import errorReducer from './errorReducer'
+import authReducer from './authReducer'
 
 
 export default combineReducers({
   items: itemReducer,
+  error: errorReducer,
+  auth: authReducer
 })
