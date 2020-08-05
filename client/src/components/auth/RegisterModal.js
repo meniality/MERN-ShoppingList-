862afa95240ce29f0ff2f99df0bea2e9f30cef54
meniality <patrh_99@hotmail.com>
2020-08-04 18:03:24 -0600

    added register user modal. Put link in navbar. added register action to authAction actions that sends axios request to backend

---
+++ client/src/components/auth/RegisterModal.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  NavLink
+} from 'reactstrap'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { register } from '../../actions/authActions'
+
+class RegisterModal extends Component {
+  state = {
+    modal: false,
+    name: '',
+    email: '',
+    password: '',
+    msg: null
+  }
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
+    register: PropTypes.func.isRequired
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  onChange = (e) => {
+    this.setState( {[e.target.name]: e.target.value})
+  }
+
+  onSubmit = (event) => {
+    
+    this.toggle()
+  }
+
+  render(props){
+    return(
+      console.log(this.props),
+      <div>
+        <NavLink onClick={this.toggle} href="#">
+          Register
+        </NavLink>
+        <Modal
+          isOpen = {this.state.modal}
+          toggle = {this.toggle}
+        >
+          <ModalHeader toggle = {this.toggle}>
+           Register 
+          </ModalHeader>
+          <ModalBody>
+            <Form onSubmit = {this.onSubmit}>
+              <FormGroup>
+                <Label for='name'>Name</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                  className='mb-3'
+                  onChange={this.onChange}
+                />
+                <Label for='name'>Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  className='mb-3'
+                  onChange={this.onChange}
+                /><Label for='name'>Password</Label>
+                <Input
+                  type="password"
+                  name="pasword"
+                  id="password"
+                  placeholder="Password"
+                  className='mb-3'
+                  onChange={this.onChange}
+                />
+                <Button
+                  color='dark'
+                  style={{marginTop: '2rem'}}
+                  block
+                >
+                Register
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error
+})
+
+export default connect(mapStateToProps, { register })(RegisterModal)
